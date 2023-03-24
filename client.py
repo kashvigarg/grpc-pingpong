49437f84f61bf0530cefc2f31e76fd543f4b24db
kashvigarg <kashvigarg02@gmail.com>
2023-03-25 01:10:03 +0530

    initial commit

---
+++ client.py
@@ -0,0 +1,36 @@
+import os
+import time
+import grpc
+import pingpong_pb2
+import pingpong_pb2_grpc
+
+def run():
+    "The run method, that sends gRPC conformant messsages to the server"
+    counter = 0
+    pid = os.getpid()
+    with grpc.insecure_channel("localhost:9999") as channel:
+        stub = pingpong_pb2_grpc.PingPongServiceStub(channel)
+        while True:
+            try:
+                start = time.time()
+                response = stub.ping(pingpong_pb2.Ping(count=counter))
+                counter = response.count
+                if counter % 1000 == 0:
+                    print(
+                        "%.4f : resp=%s : procid=%i"
+                        % (time.time() - start, response.count, pid)
+                    )
+                time.sleep(0.001)
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt")
+                channel.unsubscribe(close)
+                exit()
+
+
+def close(channel):
+    "Close the channel"
+    channel.close()
+
+
+if __name__ == "__main__":
+    run()
---
+++ pingpong_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Ping(_message.Message):
+    __slots__ = ["count"]
+    COUNT_FIELD_NUMBER: _ClassVar[int]
+    count: int
+    def __init__(self, count: _Optional[int] = ...) -> None: ...
+
+class Pong(_message.Message):
+    __slots__ = ["count"]
+    COUNT_FIELD_NUMBER: _ClassVar[int]
+    count: int
+    def __init__(self, count: _Optional[int] = ...) -> None: ...
